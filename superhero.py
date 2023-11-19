2425d355fc694b8fb3215ec5841e18c92a2e7cf2
AkiraWin <frog7bar@gmail.com>
2023-11-20 00:05:24 +0900

    a super hero

--- superhero.py
+++ superhero.py
@@ -2,18 +2,22 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 
-class myStyle(tk.Frame):
+class Superhero(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
         style = ttk.Style()
         style.theme_use('classic')
-        bg = '#333'
-        fg = '#eff'
+        bg = '#0f2537'
+        primary = '#df6919'
+        secondary = '#4e5d6c'
+        bodyfont = ('Arial', 8, 'bold')
+        fg = '#ebebeb'
         style.configure('c.TFrame', background=bg)
         style.configure('c.TLabel', background=bg, foreground=fg)  # noqa
         style.configure('c.TCheckbutton', background=bg, foreground=fg)
-        style.configure('c.TButton', borderwidth=0, background=bg, foreground=fg)  # noqa
+        style.configure('c.TButton', borderwidth=0, background=secondary, foreground=fg, font=bodyfont)  # noqa
+        style.configure('p.TButton', borderwidth=0, background=primary, foreground=fg, font=bodyfont)
         style.configure('c.TSeparator', background=bg)
         style.configure('Treeview.Heading', background=bg, foreground=fg)
         style.configure('Treeview', background=bg, foreground=fg)
@@ -24,17 +28,22 @@ class App(tk.Frame):
         super().__init__(master)
         self.pack()
 
-        self.master.title('hello')
+        self.master.title('super hello theme')
         self.master.geometry('300x300+30+30')
         self.master.resizable(width=False, height=False)
         self.master.attributes('-alpha', 0.98)
-        fm = ttk.Frame(master, width=200, height=200)
-        fm.pack()
-        lbl = ttk.Label(fm, text='spam', style='c.TLabel')
-        lbl.pack()
+        frame = ttk.Frame(master, width=200, height=200, style='c.TFrame')
+        frame.pack(expand=True, fill=tk.BOTH)
+        title_font = ('Arial', 26)
+        lbl = ttk.Label(frame, text='Superhero', style='c.TLabel', font=title_font)
+        lbl.pack(anchor=tk.W, padx=16, pady=8)
+        btn = ttk.Button(frame, text='Primary', style='p.TButton')
+        btn.pack(side=tk.LEFT, anchor=tk.N, padx=4, pady=8)
+        btn = ttk.Button(frame, text='Secondary', style='c.TButton')
+        btn.pack(side=tk.LEFT, anchor=tk.N, padx=4, pady=8)
 
 if __name__ == '__main__':
     root = tk.Tk()
-    _ = myStyle(root)
+    _ = Superhero(root)
     app = App(master=root)
     app.mainloop()
